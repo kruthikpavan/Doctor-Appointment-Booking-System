9ee3377992703c08ac9a29aa9ac579b1ad407e06
vineeshbunny <vmvr2799@gmail.com>
2022-12-17 19:22:40 -0500

    doctor Appointment booking button bug fix

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+    if (req.session.user) {
+        res.redirect("/users/home");
+        return;
+    }else if(req.session.doctors){
+        res.render("doctors/doctorhomepage");
+        return;
+    }else{  res.render('home',
+    {
+        title: 'Doctor Appointment Booking',
+    });}
     
     res.render('home',
         {
